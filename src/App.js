3e68b236ad8407f5de0143f9dc91d11e436d84f6
alvmara <alvaromarzo104@gmail.com>
2022-03-07 20:54:50 +0100

    añadiendo imagen home

--- src/App.js
+++ src/App.js
@@ -1,7 +1,20 @@
 import "./App.css";
 
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Login from "./Containers/Login/Login";
+import Home from "./Containers/Home/Home";
+
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/login" element={<Login />} />
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
 }
 
 export default App;
---
+++ src/Containers/Home/Home.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import './Home.css';
+
+const Home = () => {
+    return(
+        <div className='containerPrincipal'>
+
+        </div>
+
+    )
+}
+
+
+
+export default Home;
